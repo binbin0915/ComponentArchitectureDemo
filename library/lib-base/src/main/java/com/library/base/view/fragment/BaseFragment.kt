6a96bd30c44ee979f1377f3b4f4a9cd2853fa0be
wangkai <2925285800@qq.com>
2022-04-16 15:31:38 +0800

    去除不必要引用

--- library/lib-base/src/main/java/com/library/base/view/fragment/BaseFragment.kt
+++ library/lib-base/src/main/java/com/library/base/view/fragment/BaseFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleCoroutineScope
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
