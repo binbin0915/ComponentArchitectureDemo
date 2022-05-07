7a37b6ecb2e26682794cd973e3ddb61128dd69ac
wangkai12333 <wangkai2925@outlook.com>
2022-05-07 16:16:23 +0800

    更改项目结构

--- model/model-home/src/main/java/com/model/home/adapter/HomeViewPagerAdapter.kt
+++ model/model-home/src/main/java/com/model/home/adapter/HomeViewPagerAdapter.kt
@@ -9,12 +9,13 @@ import com.model.home.pages.HomeFragment3
 import com.model.home.pages.HomeFragment4
 
 /**
- * 作用描述：
+ * 作用描述：容纳首页fragment的ViewPager
  *
  * 创建时间：2022/03/18
  * @author：WangKai
  */
-class HomeViewPagerAdapter(activity: FragmentActivity, private val itemCount: Int) : FragmentStateAdapter(activity) {
+class HomeViewPagerAdapter(activity: FragmentActivity, private val itemCount: Int) :
+    FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
         return itemCount
     }
