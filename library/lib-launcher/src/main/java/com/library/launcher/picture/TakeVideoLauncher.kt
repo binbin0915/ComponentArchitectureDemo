40f322b5ef54d0e5bc76341fefa79d5d6484d202
wangkai <2925285800@qq.com>
2022-05-14 12:24:54 +0800

    文件访问权限

--- library/lib-launcher/src/main/java/com/library/launcher/picture/TakeVideoLauncher.kt
+++ library/lib-launcher/src/main/java/com/library/launcher/picture/TakeVideoLauncher.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Environment
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.core.content.FileProvider
 import java.io.File
 
@@ -14,36 +15,25 @@ import java.io.File
  * 创建日期： 2022/04/16
  * @author WangKai
  */
-class TakeVideoLauncher : BasePictureLauncher<Uri, Boolean>(ActivityResultContracts.CaptureVideo()) {
-    var onError: (message: String) -> Unit = {}
-    var onSuccess: (path: String) -> Unit = {}
-    var path: String = ""
+class TakeVideoLauncher :
+    BasePictureLauncher<Uri, Boolean>(ActivityResultContracts.CaptureVideo()) {
+    private var onSuccess: (path: String) -> Unit = {}
+    private var path: String = ""
 
+    @RequiresPermission(value = Manifest.permission.CAMERA)
     fun lunch(
-        onError: (message: String) -> Unit = {},
         onSuccess: (path: String) -> Unit = {}
     ) {
-        this.onError = onError
         this.onSuccess = onSuccess
-        camera.lunch(Manifest.permission.CAMERA) {
-            granted = {
-                path =
-                    "${context.getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.absolutePath}/${System.currentTimeMillis()}.mp4"
-                val fileUri =
-                    FileProvider.getUriForFile(
-                        context,
-                        "${context.applicationContext.packageName}.chooseProvider",
-                        File(path)
-                    )
-                launcher.launch(fileUri)
-            }
-            denied = {
-                onError.invoke("camera permission denied")
-            }
-            explained = {
-                onError.invoke("camera permission denied")
-            }
-        }
+        path =
+            "${context.getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.absolutePath}/${System.currentTimeMillis()}.mp4"
+        val fileUri =
+            FileProvider.getUriForFile(
+                context,
+                "${context.applicationContext.packageName}.chooseProvider",
+                File(path)
+            )
+        launcher.launch(fileUri)
     }
 
     override fun onActivityResult(result: Boolean?) {
