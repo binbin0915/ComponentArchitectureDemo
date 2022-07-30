dddb5a22d128076e8cda34c08285a1fe305bc596
wangkai <2925285800@qq.com>
2022-07-30 14:10:49 +0800

    结构优化

--- model/model-kotlin/src/main/java/com/model/mykotlin/activity/NetNormalActivity.kt
+++ model/model-kotlin/src/main/java/com/model/mykotlin/activity/NetNormalActivity.kt
@@ -1,49 +1,31 @@
 package com.model.mykotlin.activity
 
 import ando.file.core.FileDirectory
-import android.os.Bundle
 import android.util.Log
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.library.base.view.activity.BaseActivity
-import com.yupfeg.remote.download.producer.FileDownloadProducer
 import com.library.router.RouterPath
 import com.model.mykotlin.databinding.MykotlinActivityNetNormalBinding
 import com.model.mykotlin.viewmodel.NetNormalViewModel
+import com.yupfeg.remote.download.producer.FileDownloadProducer
 
 @Route(path = RouterPath.PAGE_NET_NORMAL_ACTIVITY, group = RouterPath.GROUP_KOTLIN)
 class NetNormalActivity : BaseActivity<NetNormalViewModel, MykotlinActivityNetNormalBinding>() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        /*下载文件*/
+    override fun createdObserve() {
         viewBinding.btnNormalUseDownload.setOnClickListener {
             Log.d(FileDownloadProducer.TAG, "点击下载按钮")
 
             FileDirectory.getExternalFilesDirDOWNLOADS()
                 ?.let { it1 -> viewModel.downLoad(it1.path) }
         }
-        /*协程请求接口*/
         viewBinding.btnNormalUseCoroutine.setOnClickListener {
             viewModel.queryWanAndroidArticleByCoroutine()
         }
-        /*展示结果*/
-        viewModel.articleJsonStringLiveData.observe(this) { jsonString ->
-            viewBinding.tvNormalUseResultContent.text = jsonString
-        }
-
     }
 
     override fun initData() {
-//        crashInJava()
-    }
-
-    /**
-     * 测试友盟异常捕获
-     */
-    private fun crashInJava() {
-        throw NullPointerException("Player cannot juggle swords")
-    }
-
-    override fun createdObserve() {
-
+        viewModel.articleJsonStringLiveData.observe(this) { jsonString ->
+            viewBinding.tvNormalUseResultContent.text = jsonString
+        }
     }
 }
