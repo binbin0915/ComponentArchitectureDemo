784f1d259a10c9e677d2bccb06c80c5531007b24
wangkai12333 <wangkai2925@outlook.com>
2022-08-03 15:24:07 +0800

    新的构建方式

--- versionPlugin/build.gradle.kts
+++ versionPlugin/build.gradle.kts
@@ -1,31 +1,10 @@
-buildscript {
-    repositories {
-        // 需要添加 jcenter 否则会提示找不到 gradlePlugin
-        jcenter()
-        google()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
-    }
-}
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
 }
 
-repositories {
-    mavenCentral()
-    google()
-    jcenter()
-}
-
 dependencies {
     compileOnly(gradleApi())
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
-    compileOnly("com.android.tools.build:gradle:4.1.3")
-}
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
 }
 
 gradlePlugin {
