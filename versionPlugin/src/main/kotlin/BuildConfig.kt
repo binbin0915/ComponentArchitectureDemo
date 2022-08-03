72fa408c5613fa7428726a611c3d1892c0c56722
wangkai12333 <wangkai2925@outlook.com>
2022-08-03 16:50:16 +0800

    新的构建方式

--- versionPlugin/src/main/kotlin/BuildConfig.kt
+++ versionPlugin/src/main/kotlin/BuildConfig.kt
@@ -1,28 +1,14 @@
 /**
- * @author haizhuo
+ * @author wangkai
  * @description 编译配置信息
  */
 object BuildConfig {
-    const val compileSdkVersion = 30
+    const val compileSdk = 32
     const val buildToolsVersion ="30.0.3"
-    const val minSdkVersion = 19
-    const val targetSdkVersion = 30
-
-    const val applicationId ="com.phz.composingbuilddemo"
-    const val testInstrumentationRunner="androidx.test.runner.AndroidJUnitRunner"
-
-    var versionName = "1.0"
-    var versionCode = 1
-
-    var gradle_version = "4.1.1"
-    var kotlin_version = "1.4.20"
-    val kt_stdlib get()= "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
-
-    object Kotlin {
-        var stdlib ="org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
-        val stdlibJdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
-        val stdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
-        val test = "org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version"
-        val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-    }
+    const val minSdk = 19
+    const val targetSdk = 30
+    const val applicationId ="com.wangkai.myapplication"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    const val versionName = "1.0"
+    const val versionCode = 1
 }
