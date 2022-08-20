8c1f1791a160966594f5bece9053ecec7df33e49
wangkai <2925285800@qq.com>
2022-08-21 01:14:30 +0800

    注释

--- model/model-login/src/main/java/com/model/login/data/LoginDataSource.kt
+++ model/model-login/src/main/java/com/model/login/data/LoginDataSource.kt
@@ -4,14 +4,6 @@ import kotlinx.coroutines.flow.Flow
 
 /**
  * 远程网络数据源
- *
- * 基本使用
- * *
- *
- *
- *
- *
- *
  * @author WangKai
  * @date 2022/04/03
  */
@@ -20,12 +12,10 @@ object LoginDataSource {
     private val mApiService: LoginApiService by loginApiDelegate()
 
     suspend fun queryLoginByCoroutine(username: String, password: String): LoginData{
-        //业务执行成功
         return mApiService.queryLoginByCoroutine(username, password)
     }
 
     fun queryLoginByCoroutineFlow(username: String, password: String): Flow<LoginData> {
-        //业务执行成功
         return mApiService.queryLoginByCoroutineFlow(username, password)
     }
 }
