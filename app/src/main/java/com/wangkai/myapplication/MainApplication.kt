a801712a2e94da85bcf82b705b3757800bc8d601
wangkai <2925285800@qq.com>
2022-08-24 00:28:04 +0800

    配置bugly

--- app/src/main/java/com/wangkai/myapplication/MainApplication.kt
+++ app/src/main/java/com/wangkai/myapplication/MainApplication.kt
@@ -3,7 +3,6 @@ package com.wangkai.myapplication
 import ando.file.core.FileOperator
 import android.content.Context
 import android.content.Intent
-import android.os.Environment
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
@@ -18,8 +17,9 @@ import com.tencent.bugly.Bugly
 import com.tencent.bugly.beta.Beta
 import com.tencent.bugly.beta.UpgradeInfo
 import com.tencent.bugly.beta.ui.UILifecycleListener
+import com.tencent.bugly.beta.upgrade.UpgradeListener
+import com.tencent.bugly.beta.upgrade.UpgradeStateListener
 import com.tencent.bugly.crashreport.CrashReport
-import com.tencent.bugly.crashreport.CrashReport.UserStrategy
 import com.tencent.smtt.export.external.TbsCoreSettings
 import com.tencent.smtt.sdk.QbSdk
 import com.tencent.smtt.sdk.TbsListener
@@ -112,21 +112,11 @@ class MainApplication : BaseApplication() {
 
         /*--------------------------------------Bugly相关----------------------------------------*/
         /**
-         * 参数1：上下文对象
-         * 参数2：注册时申请的APPID
-         * 参数3：是否开启debug模式，true表示打开debug模式，false表示关闭调试模式
-         */
-        Bugly.init(applicationContext, "eb89618c65", true)
-        /* 5.设置开发设备*/
-        CrashReport.setIsDevelopmentDevice(appContext, BuildConfig.DEBUG)
-        CrashReport.initCrashReport(appContext, "eb89618c65", true, setBuyConfig())
-        /**
          * 参数1：isManual 用户手动点击检查，非用户点击操作请传false
          * 参数2：isSilence 是否显示弹窗等交互，[true:没有弹窗和toast] [false:有弹窗或toast]
          */
-        Beta.checkUpgrade(false, false)
+        //Beta.checkUpgrade(false, false)
         /*Bugly Android 应用升级 SDK 高级配置*/
-
         // 1.true表示app启动自动初始化升级模块; false不会自动初始化; 开发者如果担心sdk初始化影响app启动速度，可以设置为false，在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
         Beta.autoInit = true
         // 2.设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
@@ -134,7 +124,7 @@ class MainApplication : BaseApplication() {
         // 3.true表示初始化时自动检查升级; false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
         Beta.autoCheckUpgrade = true
         // 4.设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
-        Beta.upgradeCheckPeriod = 60 * 1000
+        // Beta.upgradeCheckPeriod = 60 * 1000
         // 5.设置通知栏大图标
         Beta.largeIconId = R.mipmap.app_ic_launcher
         // 6.设置状态栏小图标
@@ -142,22 +132,22 @@ class MainApplication : BaseApplication() {
         // 7.设置更新弹窗默认展示的banner(defaultBannerId为项目中的图片资源Id; 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading...“;)
         Beta.defaultBannerId = R.mipmap.app_ic_launcher
         // 8.设置sd卡的Download为更新资源存储目录（后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;）
-        Beta.storageDir =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        // Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         // 9.设置开启显示打断策略（设置点击过确认的弹窗在App下次启动自动检查更新时会再次显示。）
         Beta.showInterruptedStrategy = true
         // 10.添加可显示弹窗的Activity(例如，只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗; 如果不设置默认所有activity都可以显示弹窗。)
         Beta.canShowUpgradeActs.add(HomeMainActivity::class.java)
         // 11.设置自定义升级对话框UI布局
         /**
-         * upgrade_dialog为项目的布局资源。 注意：因为要保持接口统一，需要用户在指定控件按照以下方式设置tag，否则会影响您的正常使用： - 特性图片：beta_upgrade_banner，如：android:tag="beta_upgrade_banner"
+         * upgrade_dialog为项目的布局资源。 注意：因为要保持接口统一，需要用户在指定控件按照以下方式设置tag，否则会影响您的正常使用：
+         * 特性图片：beta_upgrade_banner，如：android:tag="beta_upgrade_banner"
          * 标题：beta_title，如：android:tag="beta_title"
          * 升级信息：beta_upgrade_info 如： android:tag="beta_upgrade_info"
          * 更新属性：beta_upgrade_feature 如： android:tag="beta_upgrade_feature"
          * 取消按钮：beta_cancel_button 如：android:tag="beta_cancel_button"
          * 确定按钮：beta_confirm_button 如：android:tag="beta_confirm_button"
          */
-//        Beta.upgradeDialogLayoutId = R.layout.upgrade_dialog
+        Beta.upgradeDialogLayoutId = R.layout.app_upgrade_dialog
         // 12.设置自定义tip弹窗UI布局
         /**
          * 注意：因为要保持接口统一，需要用户在指定控件按照以下方式设置tag，否则会影响您的正常使用：
@@ -169,16 +159,16 @@ class MainApplication : BaseApplication() {
 //        Beta.tipsDialogLayoutId = R.layout.tips_dialog
 
         //13.设置升级对话框生命周期回调接口
-        Beta.upgradeDialogLifecycleListener = object : UILifecycleListener<UpgradeInfo?> {
-            override fun onCreate(context: Context, view: View, upgradeInfo: UpgradeInfo?) {
+        Beta.upgradeDialogLifecycleListener = object : UILifecycleListener<UpgradeInfo> {
+            override fun onCreate(context: Context, view: View, upgradeInfo: UpgradeInfo) {
 
             }
 
-            override fun onStart(context: Context, view: View, upgradeInfo: UpgradeInfo?) {
+            override fun onStart(context: Context, view: View, upgradeInfo: UpgradeInfo) {
 
             }
 
-            override fun onResume(context: Context, view: View, upgradeInfo: UpgradeInfo?) {
+            override fun onResume(context: Context, view: View, upgradeInfo: UpgradeInfo) {
                 // 注：可通过这个回调方式获取布局的控件，如果设置了id，可通过findViewById方式获取，如果设置了tag，可以通过findViewWithTag，具体参考下面例子:
 //                // 通过id方式获取控件，并更改imageview图片
 //                val imageView: ImageView = view.findViewById(R.id.icon) as ImageView
@@ -212,19 +202,55 @@ class MainApplication : BaseApplication() {
         // 14. 设置是否显示消息通知(如果你不想在通知栏显示下载进度，你可以将这个接口设置为false，默认值为true。)
         Beta.enableNotification = true
         // 15.设置Wifi下自动下载
-        Beta.autoDownloadOnWifi = false
+        Beta.autoDownloadOnWifi = true
         // 16.设置是否显示弹窗中的apk信息（如果你使用我们默认弹窗是会显示apk信息的，如果你不想显示可以将这个接口设置为false。）
         Beta.canShowApkInfo = true
         // 17.关闭热更新能力（升级SDK默认是开启热更新能力的，如果你不需要使用热更新，可以将这个接口设置为false。）
         Beta.enableHotfix = true
+        // 18.自定义Activity参考，通过回调接口来跳转到你自定义的Actiivty中。
+        Beta.upgradeListener = UpgradeListener { p0, p1, p2, p3 ->
+            if (p1 != null) {
+                val i = Intent()
+                i.setClass(
+                    appContext,
+                    UpdateActivity::class.java
+                )
+                i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                appContext.startActivity(i)
+            }
+        }
+
+        Beta.upgradeStateListener = object : UpgradeStateListener {
+            override fun onUpgradeFailed(b: Boolean) {}
+            override fun onUpgradeSuccess(b: Boolean) {}
+            override fun onUpgradeNoVersion(b: Boolean) {
+                Log.d("BUGLY_TAG", "onUpgradeNoVersion:$b")
+            }
+
+            override fun onUpgrading(b: Boolean) {
+                Log.d("BUGLY_TAG", "onUpgrading:$b")
+            }
+
+            override fun onDownloadCompleted(b: Boolean) {
+                Log.d("BUGLY_TAG", "onDownloadCompleted:$b")
+            }
+        }
+        /* 5.设置开发设备*/
+//        CrashReport.setIsDevelopmentDevice(appContext, BuildConfig.DEBUG)
+        /**
+         * 参数1：上下文对象
+         * 参数2：注册时申请的APPID
+         * 参数3：是否开启debug模式，true表示打开debug模式，false表示关闭调试模式
+         */
+        Bugly.init(applicationContext, "eb89618c65", true)
     }
 
     /**
      * 我们提供了UserStrategy类作为Bugly的初始化扩展，在这里您可以修改本次初始化Bugly数据的版本、渠道及部分初始化行为。
      * 如果通过UserStrategy设置了版本号和渠道号，则会覆盖“AndroidManifest.xml”里面配置的版本号和渠道。
      */
-    private fun setBuyConfig(): UserStrategy {
-        val strategy = UserStrategy(appContext)
+    private fun setBuyConfig(): CrashReport.UserStrategy {
+        val strategy = CrashReport.UserStrategy(appContext)
         /* 1.设置设备id*/
         /*不再采集Android id(3.4.4及之后版本)，为了使得crash率统计更精准，建议设置业务自己的deviceId（业务唯一id）给bugly sdk。3.4.4版本前默认读取Android id作为设备id，App开发者可以在初始化bugly sdk的时候设置自定义的deviceId，为了避免合规问题，请务必升级到最新合规版本。*/
         strategy.deviceID = "userdefinedId"
@@ -263,6 +289,7 @@ class MainApplication : BaseApplication() {
 上报Log最大30K。*/
         return strategy
     }
+
     /**
      * 启动X5 独立Web进程的预加载服务。优点：
      * 1、后台启动，用户无感进程切换
---
+++ app/src/main/java/com/wangkai/myapplication/UpdateActivity.kt
@@ -0,0 +1,105 @@
+package com.wangkai.myapplication
+
+import android.view.KeyEvent
+import android.view.View
+import android.widget.SeekBar
+import com.library.base.view.activity.BaseActivity
+import com.model.home.viewmodel.HomeMultiTypeBrvActivityViewModel
+import com.tencent.bugly.beta.Beta
+import com.tencent.bugly.beta.download.DownloadListener
+import com.tencent.bugly.beta.download.DownloadTask
+import com.wangkai.myapplication.databinding.AppActivityUpdateBinding
+
+
+class UpdateActivity : BaseActivity<HomeMultiTypeBrvActivityViewModel, AppActivityUpdateBinding>() {
+    fun updateBtn(task: DownloadTask) {
+        /*根据下载任务状态设置按钮*/
+        when (task.status) {
+            DownloadTask.INIT, DownloadTask.DELETED, DownloadTask.FAILED -> {
+                viewBinding.liUpdateEekBar.visibility = View.GONE
+                viewBinding.liUpdateBtn.visibility = View.VISIBLE
+                viewBinding.betaConfirmButton.text = "立即更新"
+            }
+            DownloadTask.COMPLETE -> {
+                viewBinding.liUpdateEekBar.visibility = View.GONE
+                viewBinding.liUpdateBtn.visibility = View.VISIBLE
+                viewBinding.betaConfirmButton.text = "安装"
+            }
+            DownloadTask.DOWNLOADING -> {
+                viewBinding.betaConfirmButton.text = "暂停"
+            }
+            DownloadTask.PAUSED -> {
+                viewBinding.betaConfirmButton.text = "继续下载"
+            }
+        }
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun createdObserve() {
+        viewBinding.liUpdateEekBar.visibility = View.GONE
+        viewBinding.liUpdateBtn.visibility = View.VISIBLE
+        /*获取下载任务，初始化界面信息*/
+        updateBtn(Beta.getStrategyTask())
+        /*获取策略信息，初始化界面信息*/
+        viewBinding.betaTitle.text = Beta.getUpgradeInfo().title
+        viewBinding.betaUpgradeFeature.text = Beta.getUpgradeInfo().newFeature
+        viewBinding.sbEvaluationSseekBar.isClickable = false
+        viewBinding.sbEvaluationSseekBar.isEnabled = false
+        viewBinding.sbEvaluationSseekBar.isFocusable = false
+        /*为下载按钮设置监听*/
+        viewBinding.betaConfirmButton.setOnClickListener {
+            val task = Beta.startDownload()
+            viewBinding.liUpdateEekBar.visibility = View.VISIBLE
+            viewBinding.liUpdateBtn.visibility = View.GONE
+            updateBtn(task)
+        }
+
+        /*为取消按钮设置监听*/
+        viewBinding.betaCancelButton.setOnClickListener {
+            Beta.cancelDownload()
+            finish()
+        }
+        viewBinding.sbEvaluationSseekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                viewBinding.seekText2.text = "更新进度" + progress / 10 + "%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        /*注册下载监听，监听下载事件*/Beta.registerDownloadListener(object : DownloadListener {
+            override fun onReceive(task: DownloadTask) {
+                updateBtn(task)
+                viewBinding.seekText2.visibility = View.VISIBLE
+                viewBinding.sbEvaluationSseekBar.progress = (task.savedLength * 1000 / Beta.getUpgradeInfo().fileSize).toInt()
+            }
+
+            override fun onCompleted(task: DownloadTask) {
+//                seek_text2.setText("更新进度"+(int)(task.getSavedLength()*100/Beta.getUpgradeInfo().fileSize)+"%");
+                updateBtn(task)
+                //                tv.setText(task.getSavedLength()*100/Beta.getUpgradeInfo().fileSize + "");
+            }
+
+            override fun onFailed(task: DownloadTask, code: Int, extMsg: String) {
+                updateBtn(task)
+                //                tv.setText("failed");
+            }
+        })
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        //屏蔽返回按钮
+        return if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+            true
+        } else super.onKeyDown(keyCode, event)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        /*注销下载监听*/
+        Beta.unregisterDownloadListener()
+    }
+}
