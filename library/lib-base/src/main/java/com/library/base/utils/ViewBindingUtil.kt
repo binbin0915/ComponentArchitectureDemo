8b0cb8fac057b7af45f2fbce7e3804c2caf44b96
wangkai12333 <wangkai2925@outlook.com>
2022-08-25 17:45:02 +0800

    反射获取ViewBinding

--- library/lib-base/src/main/java/com/library/base/utils/ViewBindingUtil.kt
+++ library/lib-base/src/main/java/com/library/base/utils/ViewBindingUtil.kt
@@ -1,8 +1,10 @@
 package com.library.base.utils
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import java.lang.reflect.ParameterizedType
 
@@ -10,8 +12,9 @@ import java.lang.reflect.ParameterizedType
  * Activity在onCreate中使用
  */
 @JvmName("inflateWithGeneric")
-fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInflater): VB =
-    withGenericBindingClass<VB>(this) { clazz ->
+@Suppress("UNCHECKED_CAST")
+fun <VB : ViewBinding> Activity.inflateBindingWithGeneric(layoutInflater: LayoutInflater): VB =
+    withGenericBindingClass(this) { clazz ->
         clazz.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as VB
     }
 
@@ -19,9 +22,10 @@ fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInfla
  * Fragment在onCreateView使用
  */
 @JvmName("inflateWithGeneric")
-fun <VB : ViewBinding> Any.inflateBindingWithGeneric(
+@Suppress("UNCHECKED_CAST")
+fun <VB : ViewBinding> Fragment.inflateBindingWithGeneric(
     layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean
-): VB = withGenericBindingClass<VB>(this) { clazz ->
+): VB = withGenericBindingClass(this) { clazz ->
     clazz.getMethod(
         "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
     ).invoke(null, layoutInflater, parent, attachToParent) as VB
@@ -31,33 +35,47 @@ fun <VB : ViewBinding> Any.inflateBindingWithGeneric(
  * 在自定义ViewGroup（包括Recyclerview）中使用
  */
 @JvmName("inflateWithGeneric")
-fun <VB : ViewBinding> Any.inflateBindingWithGeneric(parent: ViewGroup): VB =
-    inflateBindingWithGeneric(LayoutInflater.from(parent.context), parent, false)
+@Suppress("UNCHECKED_CAST")
+fun <VB : ViewBinding> ViewGroup.inflateBindingWithGeneric(parent: ViewGroup): VB =
+    withGenericBindingClass(this) { clazz ->
+        clazz.getMethod(
+            "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
+        ).invoke(null, LayoutInflater.from(parent.context), parent, false) as VB
+    }
+
 
 /**
  * 在自定义View中使用
  */
-fun <VB : ViewBinding> Any.bindViewWithGeneric(view: View): VB =
+@JvmName("inflateWithGeneric")
+@Suppress("UNCHECKED_CAST")
+fun <VB : ViewBinding> View.bindViewWithGeneric(view: View): VB =
     withGenericBindingClass<VB>(this) { clazz ->
         clazz.getMethod("bind", View::class.java).invoke(null, view) as VB
     }
 
-
+/**
+ * 获取ViewBinding
+ */
+@Suppress("UNCHECKED_CAST")
 private fun <VB : ViewBinding> withGenericBindingClass(any: Any, block: (Class<VB>) -> VB): VB {
     any.allParameterizedType.forEach { parameterizedType ->
         parameterizedType.actualTypeArguments.forEach {
             try {
                 return block.invoke(it as Class<VB>)
-            } catch (e: NoSuchMethodException) {
-                e.printStackTrace()
-            } catch (e: ClassCastException) {
-                e.printStackTrace()
+            } catch (_: NoSuchMethodException) {
+            } catch (_: ClassCastException) {
             }
         }
     }
     throw IllegalArgumentException("There is no generic of ViewBinding.")
 }
 
+/**
+ * ParameterizedType：参数化类型
+ * getGenericSuperclass()：获得带有泛型的父类
+ * Type是 Java 编程语言中所有类型的公共高级接口。它们包括原始类型、参数化类型、数组类型、类型变量和基本类型。
+ */
 private val Any.allParameterizedType: List<ParameterizedType>
     get() {
         val genericParameterizedType = mutableListOf<ParameterizedType>()
