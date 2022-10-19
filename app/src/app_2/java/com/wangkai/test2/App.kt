155624386039b2aa6641379b8da19c33e0e2b644
wangkai <2925285800@qq.com>
2022-10-19 22:58:57 +0800

    更新配置

--- app/src/app_2/java/com/wangkai/test2/App.kt
+++ app/src/app_2/java/com/wangkai/test2/App.kt
@@ -1,4 +1,13 @@
 package com.wangkai.test2
 
 class App {
+    companion object {
+        //ACRA配置开始
+        const val ACRA_HTTP = "http://39.106.144.234:8080/report"
+        const val ACRA_AuthLogin = "MqFkSEe9iGFrcAom"
+        const val ACRA_AuthPassword = "yM2FZ3DdCOetdxxh"
+
+        //更新的配置开始
+        const val Update_URL = "https://ceshi.new.beisu100.com/api/downapk/getapkinfo"
+    }
 }
