7a2e527d9aeab8c14efd49819d315980e55a8628
wangkai12333 <wangkai2925@outlook.com>
2022-10-20 17:48:49 +0800

    jetpack compose

--- model/model-compose/src/main/java/com/wangkai/compose/ui/layout/Greeting.kt
+++ model/model-compose/src/main/java/com/wangkai/compose/ui/layout/Greeting.kt
@@ -1,13 +1,25 @@
 package com.wangkai.compose.ui.layout
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wangkai.compose.ui.theme.MyApplicationTheme
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .width(150.dp)
+            .clickable {
+
+
+            })
 }
 
 @Preview(showBackground = true)
