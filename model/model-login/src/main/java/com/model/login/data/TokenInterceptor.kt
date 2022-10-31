0a28036df22be648419a728f020cecaffd3ee441
wangkai12333 <wangkai2925@outlook.com>
2022-10-31 17:52:05 +0800

    dataStore优化

--- model/model-login/src/main/java/com/model/login/data/TokenInterceptor.kt
+++ model/model-login/src/main/java/com/model/login/data/TokenInterceptor.kt
@@ -6,6 +6,7 @@ import com.library.base.datastore.DataStoreUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -23,7 +24,10 @@ class TokenInterceptor : Interceptor {
         val url = request.url.toString()
         if (url.isNotEmpty() && url.contains("/api/login")) {
             //登录接口获取token并保存
-            CoroutineScope(Dispatchers.IO).launch {
+            //请尽可能避免在 DataStore 数据读取时阻塞线程。阻塞界面线程可能会导致 ANR 或界面卡顿，而阻塞其他线程可能会导致死锁。
+            //DataStore 的主要优势之一是异步 API，但可能不一定始终能将周围的代码更改为异步代码。如果您使用的现有代码库采用同步磁盘 I/O，或者您的依赖项不提供异步 API，就可能出现这种情况。
+            //Kotlin 协程提供 runBlocking() 协程构建器，以帮助消除同步与异步代码之间的差异。您可以使用 runBlocking() 从 DataStore 同步读取数据。
+            runBlocking {
                 response.headers["Authorization"]?.run {
                     DataStoreUtils.put(BaseApplication.appContext, "Authorization", this)
                 }
